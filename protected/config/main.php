2aab98d98af14eb829b9e905d4ff0cd8234f2bfc
Navin <navininfotech.2014@gmail.com>
2018-02-15 17:04:48 +0530

    Test Changes
    Signed-off-by: Navin <navininfotech.2014@gmail.com>

--- protected/config/main.php
+++ protected/config/main.php
@@ -3,7 +3,7 @@
 return array(
     'basePath'          => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name'              => 'College Final Project',
-   // 'defaultController' => 'post/index',
+   //   'defaultController' => 'post/index',
    // 'homeUrl'           => 'site/login',
     
     'defaultController' => 'site/login',
